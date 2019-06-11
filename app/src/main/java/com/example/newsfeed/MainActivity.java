7f6c3524b19f0d06b30c0fa8f873754992d9e50d
jocavuleta <jocavuleta@email.com>
2019-06-11 10:17:38 +0200

    Error Hanndling Retrofit

--- app/src/main/java/com/example/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -19,9 +19,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.newsfeed.api.ApiClient;
 import com.example.newsfeed.api.ApiInterface;
@@ -45,6 +46,10 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     private String TAG = MainActivity.class.getSimpleName();
     private TextView topHeadline;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private RelativeLayout errorLayout;
+    private ImageView errorImage;
+    private TextView errorTitle, errorMessage;
+    private Button btnRetry;
 
     @SuppressLint("ResourceAsColor")
     @Override
@@ -64,12 +69,20 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         recyclerView.setNestedScrollingEnabled(false);
 
         onLoadingSwipeRefresh("");
+
+        errorLayout = findViewById(R.id.errorLayout);
+        errorImage = findViewById(R.id.errorImage);
+        errorTitle = findViewById(R.id.errorTitle);
+        errorMessage = findViewById(R.id.errorMessage);
+        btnRetry = findViewById(R.id.btnRetry);
     }
 
     public void LoadJson(final String keyword){
 
+        errorLayout.setVisibility(View.GONE);
         topHeadline.setVisibility(View.INVISIBLE);
         swipeRefreshLayout.setRefreshing(true);
+
 
         ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
 
@@ -108,7 +121,25 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
                     topHeadline.setVisibility(View.INVISIBLE);
                     swipeRefreshLayout.setRefreshing(false);
-                    Toast.makeText(MainActivity.this, "No Result!", Toast.LENGTH_SHORT).show();
+
+                    String errorCode;
+                    switch (response.code()){
+                        case 404:
+                            errorCode = "404 not found";
+                            break;
+                        case 500:
+                            errorCode = "500 server broken";
+                            break;
+                        default:
+                            errorCode = "unknown error";
+                            break;
+                    }
+
+                    showErrorMessage(
+                            R.drawable.no_result,
+                            "No Result",
+                            "Please Try Again!\n" +
+                            errorCode);
                 }
             }
 
@@ -116,6 +147,11 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             public void onFailure(Call<News> call, Throwable t) {
                 topHeadline.setVisibility(View.INVISIBLE);
                 swipeRefreshLayout.setRefreshing(false);
+                showErrorMessage(
+                        R.drawable.no_result,
+                        "Oops..",
+                        "Network failure, Please Try Again!\n" +
+                                t.toString());
 
             }
         });
@@ -202,4 +238,24 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 }
         );
     }
+
+    private void showErrorMessage(int imageView,String title, String message){
+
+        if (errorLayout.getVisibility() == View.GONE) {
+            errorLayout.setVisibility(View.VISIBLE);
+
+        }
+        errorImage.setImageResource(imageView);
+        errorTitle.setText(title);
+        errorMessage.setText(message);
+
+        btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoadingSwipeRefresh("");
+            }
+        });
+
+    }
+
 }
